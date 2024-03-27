1af82e11da66174df883db5947396342e1d3973e
Junbal <oh971021@gmail.com>
2024-03-28 00:16:42 +0900

    first commit

---
+++ src/test/java/akatsuki/mk/MkApplicationTests.java
@@ -0,0 +1,13 @@
+package akatsuki.mk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
